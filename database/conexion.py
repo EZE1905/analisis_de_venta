add541e8aca0a27a7486329634b18fa7d37ad2ff
Ezequiel Albornoz <eze_thomi@hotmail.com>
2026-05-12 12:22:43 -0300

    proyecto analizador de ventas

---
+++ database/conexion.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+def conectar_base_datos():
+    try:
+        conexion = psycopg2.connect(
+            database="sistema_ventas",
+            user="postgres",
+            password="ezesql",
+            host="localhost",
+            port="5432"
+        )
+        print("Conexión exitosa a la base de datos")
+    except Exception as e:
+        print(f"Error al conectar a la base de datos: {e}")
+    return conexion
+
+def cerrar_conexion(conexion):
+    if conexion:
+        conexion.close()
+        print("Conexión cerrada")
